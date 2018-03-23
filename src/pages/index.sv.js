c22a4fdb0005fe9153df33e0b97b0c04ea15b8e8
Robin Pahlman <robin.pahlman@gmail.com>
2018-03-23 14:45:40 -0400

    add plugin styled components, set up flex container, add hero img

--- src/pages/index.sv.js
+++ src/pages/index.sv.js
@@ -1,13 +1,37 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import styled from 'styled-components'
+import andersHero from '../assets/anders-hero-3-gradient.jpg'
+
+const Container = styled.div`
+  display: flex; 
+  min-height: 100vh;
+  flex-flow: row wrap;
+`;
+
+const Hero = styled.section`
+  background-image: url(${andersHero});
+  background-size: cover;
+  background-postition: center; 
+  margin: auto; 
+  width: 100vh; 
+  min-height: 70vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  flex: 1 100%;
+  padding: 20px;
+`;
 
 const IndexPage = () => (
-  <div>
-    <h1>Hej hej</h1>
-    <p>Välkomment till Gatsby.</p>
-    <p>Testing testing.</p>
-    <Link to="/sv/page-2/">Till sid 2</Link>
-  </div>
+  <Container>
+    <Hero>
+      <h1>Hej hej</h1>
+      <p>Välkomment till Gatsby.</p>
+      <p>Testing testing.</p>
+      <Link to="/sv/page-2/">Till sid 2</Link>
+    </Hero>
+  </Container>
 )
 
 export default IndexPage
